e74bfb2f0cebd10f94f7588926ee82f77879a037
Amaitari Hezekiah Philip <amaitarihezekiah@gmail.com>
2020-09-20 01:25:13 +0100

    chore: (typeScript) add typeScript support

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head'
+
+const HomePage: React.FC = () => {
+    return (
+        <>
+            <Head>
+                <title>3reenshop</title>
+            </Head>
+            <h1>Welcome to 3reen e-commerce app - 3reenshop</h1></>
+    )
+}
+
+export default HomePage;
