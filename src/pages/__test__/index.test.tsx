9bc1fb0f38f403be6b373a23f32d92e47ed9f3fc
Amaitari Hezekiah Philip <amaitarihezekiah@gmail.com>
2020-09-22 00:03:16 +0100

    test(index): change getByText query to getByRole query

--- src/pages/__test__/index.test.tsx
+++ src/pages/__test__/index.test.tsx
@@ -5,6 +5,8 @@ import HomePage from '../index';
 describe('<App />', () => {
     it('renders HomePage component correctly', () => {
         render(<HomePage />);
-        expect(screen.getByText('Welcome to 3reen e-commerce app - 3reenshop')).toBeInTheDocument();
+        expect(
+            screen.getByRole('heading', { name: /Welcome to 3reen e-commerce app - 3reenshop/i }),
+        ).toBeInTheDocument();
     });
 });
