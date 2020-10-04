447fb710535357242c340b0d136154ec7f3e3d3e
Amaitari Hezekiah Philip <41383449+Biosmatrix@users.noreply.github.com>
2020-10-04 08:38:48 +0100

    Chore(workflow): update release workflow with github actions (#21)
    * chore: remove semantic-release and dependencies
    
    * chore(shipjs): add shipjs, config and dependencies
    
    * build: refine release job

---
+++ ship.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    buildCommand: () => 'yarn build',
+    testCommandBeforeRelease: () => 'yarn test:ci',
+    afterPublish: ({ exec }) => {
+        exec(`git config --global user.email "amaitarihezekiah@gmail.com"`);
+        exec(`git config --global user.name "Biosmatrix"`);
+
+        exec('git checkout develop');
+        exec('git merge master');
+        exec('git push origin develop');
+    },
+};
