7057cc17cc4fb7322ff90048a809d2c1632f5136
Amaitari Hezekiah Philip <41383449+Biosmatrix@users.noreply.github.com>
2020-10-30 08:06:53 +0100

    fix: remove lighthouse ci (#36)
    * fix: add github token to allow lhci to upload audit to server
    
    * ci: add deployment preview URL lighthouse audit
    
    * ci: remove lhci assertions rules
    
    * ci: remove lighthouse ci step from job workflow
    
    * ci: add heroku review app ci workflow
    
    * chore: move less-vars-to-js, raw-loader from devDependencies to dependencies
    
    * ci: fix lighthouse ci error
    
    * ci: remove lighthouse ci

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,28 +3,6 @@ module.exports = {
         collect: {
             staticDistDir: '.next',
         },
-        assert: {
-            preset: 'lighthouse:recommended',
-            assertions: {
-                'offscreen-images': 'off',
-                'uses-webp-images': 'off',
-                'color-contrast': 'off',
-
-                'apple-touch-icon': 'off',
-                'maskable-icon': 'off',
-                'installable-manifest': 'off',
-                'offline-start-url': 'off',
-                'service-worker': 'off',
-                'splash-screen': 'off',
-                'themed-omnibox': 'off',
-                'works-offline': 'off',
-
-                'document-title': 'off',
-                'errors-in-console': 'off',
-                'html-has-lang': 'off',
-                'meta-description': 'off',
-            },
-        },
         upload: {
             target: 'temporary-public-storage',
         },
