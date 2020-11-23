aaa46685db6590e183b78f69c08212eeb5a563e0
Nkwor-Jane <69125833+Nkwor-Jane@users.noreply.github.com>
2020-11-23 22:26:21 +0100

    feat(products): add products page

---
+++ src/pages/products.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+
+function products() {
+    return (
+        <>
+            <Header />
+            <h1>PRODUCTS PAGES</h1>
+            <Footer />
+        </>
+    );
+}
+
+export default products;
