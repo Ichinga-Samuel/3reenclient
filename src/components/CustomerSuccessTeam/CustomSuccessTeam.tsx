4d420c62d1664585f454f8cb0fcafba494c510d7
chris097 <chrisfidel.international@gmail.com>
2020-12-30 16:13:17 +0100

    feat: add view product page

--- src/components/CustomerSuccessTeam/CustomSuccessTeam.tsx
+++ src/components/CustomerSuccessTeam/CustomSuccessTeam.tsx
@@ -10,13 +10,22 @@ import notifyIcon from './Icons/notify-icon.png';
 import logOut from './Icons/Log out.svg';
 import avatar from './Icons/Avatar.svg';
 import boxImg from './Images/Box-img.svg';
+import viewDots from './Icons/view-dots.svg';
+
+export default {
+    title: 'img',
+};
+
+const image = {
+    cstLogo: cstLogo,
+};
 
 export const CustomerSuccessTeamSidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar-content">
                 <div className="sidebar-logo">
-                    <img src={cstLogo} alt="" />
+                    <img src={image.cstLogo} alt="" />
                 </div>
                 <button className="btn-primary">
                     <img src={clipBoard} alt="" />
@@ -35,31 +44,117 @@ export const CustomerSuccessTeamSidebar = () => {
     );
 };
 
+export const UserId = () => {
+    return (
+        <div className="user-id">
+            <img src={avatar} alt="userPhoto" />
+            <div className="user-details">
+                <div className="user-name">Jack Hanniel</div>
+                <div className="sales-rep">Sales Rep.</div>
+            </div>
+        </div>
+    );
+};
+
+export const MainWrapper = () => {
+    return (
+        <div className="cst-details">
+            <div className="prod-info-content">Product Information</div>
+            <div className="prod-info-box">
+                <div className="column">
+                    <div className="prod-items">
+                        <img src={boxImg} alt="" />
+                        <div>
+                            <h4>Fashion Bags</h4>
+                            <p>#739339</p>
+                        </div>
+                    </div>
+                    <div className="prod-view-dots">
+                        <img src={viewDots} alt="" />
+                        <ul className="view-prod-btn">
+                            <li>View Product</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="prod-info-box">
+                <div className="column">
+                    <div className="prod-items">
+                        <img src={boxImg} alt="" />
+                        <div>
+                            <h4>Fashion Bags</h4>
+                            <p>#739339</p>
+                        </div>
+                    </div>
+                    <div className="prod-view-dots">
+                        <img src={viewDots} alt="" />
+                        <ul className="view-prod-btn">
+                            <li>View Product</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="pagination">
+                <button className="page current">1</button>
+                <button className="page">2</button>
+                <button className="page">3</button>
+            </div>
+        </div>
+    );
+};
+
 export const CustomerSuccessTeam = () => {
     return (
         <>
-            <div className="column">
-                <CustomerSuccessTeamSidebar />
-                <div className="cst-details">
-                    <div className="prod-info-content">Product Information</div>
-                    <div className="user-id">
-                        <img src={avatar} alt="userPhoto" />
-                        <div className="user-details">
-                            <div className="user-name">Jack Hanniel</div>
-                            <div className="sales-rep">Sales Rep.</div>
+            <CustomerSuccessTeamSidebar />
+            <UserId />
+            {/* <MainWrapper /> */}
+            <div className="cst-details">
+                <button className="exit-view_product">X</button>
+                <div className="view-product_content">
+                    <div className="column">
+                        <div className="view-product_columnA">
+                            <img src={boxImg} alt="" />
+                        </div>
+                        <div className="view-product_columnB">
+                            <div className="product-tags">
+                                <div className="product-tag_name">Fashion Bags</div>
+                                <div className="product-tag_price">$150.00</div>
+                                <div className="product-tag_id">#26289</div>
+                                <div className="product-tag_type">Female Accessories</div>
+                                <div className="product-tag_location">Abuja</div>
+                            </div>
                         </div>
                     </div>
-
-                    <div className="prod-info-box">
-                        <div className="column">
-                            <div className="col">
-                                <img src={boxImg} alt="" />
-                                <div className="items">
-                                    <p>Fashion Bags</p>
-                                    <p>#289484</p>
-                                </div>
+                </div>
+                <div className="view-product_content">
+                    <div className="column">
+                        <div className="view-product_columnA">
+                            <div className="view-product_description">
+                                <h3 className="primary-color">Description</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
+                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor. Imperdiet quis
+                                    turpis vel commodo tincidunt. Elit turpis quis amet morbi sed tristique non tellus
+                                    nunc. Pharetra a lectus gravida elementum risus quam quam sodales. ed tristique non
+                                    tellus nunc. Pharetra a lectus gravida elementum risus quam quam sodales.
+                                </p>
                             </div>
-                            <div className="b">B</div>
+                        </div>
+                        <div className="view-product_columnB">
+                            <div className="view-product_description">
+                                <h3 className="primary-color">Key Features</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
+                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor.
+                                </p>
+
+                                <h3 className="primary-color mgtop20">Specifications</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
+                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
