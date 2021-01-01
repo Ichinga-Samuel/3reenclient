e1e00d6d8896c9cb8cdcabf1fce31d05653c2608
chris097 <chrisfidel.international@gmail.com>
2021-01-01 09:54:14 +0100

    feat: add cst third page

--- src/components/CustomerSuccessTeam/CustomSuccessTeam.tsx
+++ src/components/CustomerSuccessTeam/CustomSuccessTeam.tsx
@@ -6,11 +6,11 @@ import './CustomerSuccessTeam.css';
 import './Utils.css';
 import cstLogo from './Icons/CST-logo.svg';
 import clipBoard from './Icons/clipboard.svg';
-import notifyIcon from './Icons/notify-icon.png';
 import logOut from './Icons/Log out.svg';
 import avatar from './Icons/Avatar.svg';
 import boxImg from './Images/Box-img.svg';
 import viewDots from './Icons/view-dots.svg';
+import exitViewProduct from './Icons/exit-view-product.svg';
 
 export default {
     title: 'img',
@@ -32,7 +32,7 @@ export const CustomerSuccessTeamSidebar = () => {
                     <span>Production Information</span>
                 </button>
                 <button className="btn-secondary">
-                    <img src={notifyIcon} alt="" />
+                    {/* <img src={} alt="" /> */}
                     <span>Order Notification</span>
                 </button>
                 <button className="btn-tertiary">REGISTER CUSTOMERS</button>
@@ -50,7 +50,7 @@ export const UserId = () => {
             <img src={avatar} alt="userPhoto" />
             <div className="user-details">
                 <div className="user-name">Jack Hanniel</div>
-                <div className="sales-rep">Sales Rep.</div>
+                <div className="sales-rep">Customer Success Team</div>
             </div>
         </div>
     );
@@ -103,60 +103,112 @@ export const MainWrapper = () => {
     );
 };
 
+export const ViewProduct = () => {
+    return (
+        <div className="cst-details_view-product">
+            <button className="exit-view_product">
+                <img src={exitViewProduct} alt="" />
+            </button>
+            <div className="view-product_content">
+                <div className="column">
+                    <div className="view-product_columnA">
+                        <img src={boxImg} alt="" />
+                    </div>
+                    <div className="view-product_columnB">
+                        <div className="product-tags">
+                            <div className="product-tag_name">Fashion Bags</div>
+                            <div className="product-tag_price">$150.00</div>
+                            <div className="product-tag_id">#26289</div>
+                            <div className="product-tag_type">Female Accessories</div>
+                            <div className="product-tag_location">Abuja</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="view-product_columnA">
+                        <div className="view-product_description">
+                            <h3 className="primary-color">Description</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque libero
+                                eget cursus. Est in eget vitae congue fermentum eget tortor. Imperdiet quis turpis vel
+                                commodo tincidunt. Elit turpis quis amet morbi sed tristique non tellus nunc. Pharetra a
+                                lectus gravida elementum risus quam quam sodales. ed tristique non tellus nunc. Pharetra
+                                a lectus gravida elementum risus quam quam sodales. turpis vel commodo tincidunt. Elit
+                                turpis quis amet morbi sed tristique non tellus nunc. Pharetra a lectus gravida
+                                elementum risus quam quam sodales. ed tristique non tellus nunc. Pharetra a lectus
+                                gravida elementum risus quam quam sodales.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="view-product_columnB">
+                        <div className="view-product_description">
+                            <h3 className="primary-color">Key Features</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque libero
+                                eget cursus. Est in eget vitae congue fermentum eget tortor.
+                            </p>
+
+                            <h3 className="primary-color mgtop20">Specifications</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque libero
+                                eget cursus. Est in eget vitae congue fermentum eget tortor. turpis vel commodo
+                                tincidunt. Elit turpis quis amet morbi sed tristique non tellus nunc. Pharetra a lectus
+                                gravida elementum risus quam quam sodales. ed tristique non tellus nunc. Pharetra a
+                                lectus gravida elementum risus quam quam sodales.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="view-availability">
+                    <h3 className="primary-color">Availability</h3>
+                    <div className="col">
+                        <p>Is this product available</p>
+                        <button className="view-btn-yes">Yes</button>
+                        <button className="view-btn-no">No</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export const CustomerSuccessTeam = () => {
     return (
         <>
             <CustomerSuccessTeamSidebar />
             <UserId />
             {/* <MainWrapper /> */}
-            <div className="cst-details">
-                <button className="exit-view_product">X</button>
-                <div className="view-product_content">
-                    <div className="column">
-                        <div className="view-product_columnA">
-                            <img src={boxImg} alt="" />
-                        </div>
-                        <div className="view-product_columnB">
-                            <div className="product-tags">
-                                <div className="product-tag_name">Fashion Bags</div>
-                                <div className="product-tag_price">$150.00</div>
-                                <div className="product-tag_id">#26289</div>
-                                <div className="product-tag_type">Female Accessories</div>
-                                <div className="product-tag_location">Abuja</div>
-                            </div>
-                        </div>
-                    </div>
+            {/* <ViewProduct /> */}
+            <div className="cst-details_view-product">
+                <div className="order-info-content">Order Notification</div>
+                <div className="column">
+                    <p>Delivery Details</p>
+                    <p>Customer Review</p>
                 </div>
-                <div className="view-product_content">
-                    <div className="column">
-                        <div className="view-product_columnA">
-                            <div className="view-product_description">
-                                <h3 className="primary-color">Description</h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
-                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor. Imperdiet quis
-                                    turpis vel commodo tincidunt. Elit turpis quis amet morbi sed tristique non tellus
-                                    nunc. Pharetra a lectus gravida elementum risus quam quam sodales. ed tristique non
-                                    tellus nunc. Pharetra a lectus gravida elementum risus quam quam sodales.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="view-product_columnB">
-                            <div className="view-product_description">
-                                <h3 className="primary-color">Key Features</h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
-                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor.
-                                </p>
-
-                                <h3 className="primary-color mgtop20">Specifications</h3>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Diam nunc sem quisque
-                                    libero eget cursus. Est in eget vitae congue fermentum eget tortor.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                <div className="order-product_content">
+                    <table>
+                        <tr>
+                            <th>Date</th>
+                            <th>Product</th>
+                            <th>Qty</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>State</th>
+                            <th>LGA</th>
+                            <th>Phone No.</th>
+                        </tr>
+                        <tr>
+                            <th>19/20/2020</th>
+                            <th>Macbook Air</th>
+                            <th>1</th>
+                            <th>John Joe</th>
+                            <th>123 Lafemi Street, beside Mobil Oil</th>
+                            <th>Lagos</th>
+                            <th>Ikeja</th>
+                            <th>+234802345678</th>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </>
