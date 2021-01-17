7c0fb2f50ef9cf827dba3b118926223402a352b0
chris097 <chrisfidel.international@gmail.com>
2021-01-17 22:04:09 +0100

    feat: updating app

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
-import '../assets/styles/main.scss';
-
-function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
