548954b242169ea581326dab80f397affa32fa55
Falua Temitope Oyewole <emmat0616@gmail.com>
2021-06-11 13:46:48 +0100

    feat(qa): layout redesign

--- src/components/QualityAssurance/QASideMenu/QASideMenu.tsx
+++ src/components/QualityAssurance/QASideMenu/QASideMenu.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { UserOutlined, VideoCameraOutlined, UploadOutlined } from '@ant-design/icons';
+import { UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import Link from 'next/link';
+import { LogoWhiteIcon } from '@/utils/Icons';
 const { Sider } = Layout;
 const QASideMenu = (props) => {
     const { collapsed } = props;
     console.log(collapsed);
     return (
         <Sider trigger={null} collapsible collapsed={collapsed}>
-            <div className="logo" />
+            <div className="logo">
+                <LogoWhiteIcon />
+            </div>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
                 <Menu.Item key="1" icon={<UserOutlined />}>
-                    nav 1
+                    <Link href="">Orders</Link>
                 </Menu.Item>
                 <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                    nav 2
-                </Menu.Item>
-                <Menu.Item key="3" icon={<UploadOutlined />}>
-                    nav 3
+                    <Link href="">Messages</Link>
                 </Menu.Item>
             </Menu>
         </Sider>
