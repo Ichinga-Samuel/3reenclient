257c84e99bdcf826629e61fdad22720873500dff
Viczdera <vichizsoft@yahoo.com>
2021-07-05 12:19:27 +0100

    admin login and pass reset

---
+++ src/components/Admin/AdminLayout/AdminOrdersDisplay.js
@@ -0,0 +1,23 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { USER } from '@/utils/ApiList';
+import { addToLocalStorage, getFromLocalStorage } from '@/utils/browserStorage';
+import AdminHomeLayout from './AdminHomeLayout';
+import AdminHeader from './AdminHeader';
+
+const AdminOrdersDisplay = () => {
+    
+    return (
+        <>
+            <AdminHeader/>
+            <AdminHomeLayout pageTitle="OrdersDisplay">
+                <div className="login" data-aos="slide-right" data-aos-delay="2s" data-aos-duration="1s">
+                    OrdersDisplay
+                </div>
+            </AdminHomeLayout>
+        </>
+    );
+};
+
+
+export default AdminOrdersDisplay;
---
+++ src/pages/admin/customer_service.js
@@ -0,0 +1,12 @@
+import AdminCustomerService from '@/components/Admin/AdminLayout/AdminCustomerService';
+import AdminInventory from '@/components/Admin/AdminLayout/AdminInventory';
+import React from 'react';
+const customer_service=()=> {
+    return (
+        <div>
+            <AdminCustomerService/>
+        </div>
+    );
+}
+
+export default customer_service;
