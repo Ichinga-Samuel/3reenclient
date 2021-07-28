8c9a1d31be7d8218075cf5b3e0364a812e386234
Falua Temitope O <emmat0616@gmail.com>
2021-07-28 22:15:11 +0100

    Updated Details

--- src/components/SuperAdmin/AllAdminUsers.tsx
+++ src/components/SuperAdmin/AllAdminUsers.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import SuperDefaultLayout from '@/components/SuperAdmin/Layout/SuperDefaultLayout';
-import { Col, Row } from 'antd';
+import { Button, Col, Input, Row, Table } from 'antd';
+import { GeneralContainer } from '@/components/Admin/styles/AdminStyle.styled';
 
 const AllAdminUsers = () => {
     return (
         <SuperDefaultLayout browserTitle="All Admin Users" breadTitle="All Admin Users">
-            <Row gutter={28}>
-                <Col>3</Col>
+            <Row gutter={28} justify="space-between">
+                <Col xs={24} xl={12} lg={12}>
+                    <Row gutter={28}>
+                        <Col xs={10} xl={8} lg={8}>
+                            <Button type="primary" block>
+                                Add New Admin
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col xs={24} xl={10} lg={10}>
+                    <div style={{ display: 'flex' }}>
+                        <Input placeholder="Search 'Admin Users'" style={{ marginRight: '5px' }} />
+                        <Button type="primary">Search</Button>
+                    </div>
+                </Col>
             </Row>
+            <GeneralContainer style={{ marginTop: '15px' }}>
+                <Table />
+            </GeneralContainer>
         </SuperDefaultLayout>
     );
 };
