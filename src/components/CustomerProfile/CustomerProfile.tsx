e03baeed2f0650075d8a176a486d7fd1d94b5258
Tolanee <68304958+Tolanee@users.noreply.github.com>
2022-01-05 13:15:46 +0100

    Profile and login

--- src/components/CustomerProfile/CustomerProfile.tsx
+++ src/components/CustomerProfile/CustomerProfile.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProfileWrap } from './styled/CustomerProfileStyled';
+import { ProfileWrap} from './styled/CustomerProfileStyled';
 import editPen from './Icons/Edit-pen.svg';
 //import axios from "axios"
 import { useForm } from 'react-hook-form';
@@ -24,51 +24,50 @@ const Profile = () => {
                     <form onSubmit={Submit} className="profile-box">
                         <div className="edit-text">Edit your informations below</div>
                         <div className="user-form">
-                            <div className="mgtop15">
-                                <label htmlFor="fullname">FULL NAME</label>
+                            {/* <div className="mgtop15 info"> */}
+                                {/* <label htmlFor="fullname">FULL NAME</label><br/> */}
                                 <input
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    placeholder="Full Name"
-                                    {...register('fullName', { required: true })}
-                                />
-                                {errors.fullName && <span className="error">Full Name is required</span>}
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Full Name"
+                                {...register('fullName', { required: true })}/> 
+                                {errors.fullName && <span className="error">Full Name Required</span>}
 
                                 <button type="reset">
                                     <img src={editPen} alt="" />
                                 </button>
-                            </div>
-                            <div className="mgtop15">
-                                <label htmlFor="email">EMAIL ADDRESS</label>
+                            {/* </div> */}
+                            {/* <div className="mgtop15 info"> */}
+                                {/* <label htmlFor="email">EMAIL ADDRESS</label><br/> */}
                                 <input
+                                    className='input'
                                     type="email"
                                     name="email"
                                     id="email"
-                                    placeholder="johndoe@gmail.com"
+                                    placeholder="Email"
                                     {...register('email', {
                                         required: true,
                                         pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
                                     })}
-                                />
-                                {errors.email && <span className="error">enter a valid email</span>}
+                                />{errors.email && <span className="error">Enter valid email</span>}
 
-                                <span className="opt">
+                                {/* <span className="opt">
                                     <button type="submit" className="col-green">
                                         (email address comfirmed,
                                     </button>
                                     <button type="submit" className="primary-color">
                                         change email address)
                                     </button>
-                                </span>
-                            </div>
-                            <div className="mgtop15">
-                                <label htmlFor="fullname">PASSWORD</label>
+                                </span> */}
+                            {/* </div> */}
+                            {/* <div className="mgtop15 info"> */}
+                                {/* <label htmlFor="fullname">PASSWORD</label><br/> */}
                                 <input
                                     type="text"
                                     name="password"
                                     id="password"
-                                    placeholder="*******"
+                                    placeholder="Password"
                                     {...register('password', {
                                         required: true,
                                         minLength: {
@@ -83,82 +82,82 @@ const Profile = () => {
                                 <button type="reset">
                                     <img src={editPen} alt="" />
                                 </button>
-                            </div>
-                            <div className="mgtop15 flex-box">
-                                <label htmlFor="contact">CONTACT ADDRESS</label>
+                            {/* </div> */}
+                            <div className="mgtop15 ">
+                                <label htmlFor="contact">CONTACT ADDRESS</label><br/>
                                 <input
                                     type="text"
                                     name="streetAdd"
                                     id="streetAdd"
-                                    placeholder="STREET ADDRESS"
+                                    placeholder="Street Address"
                                     className="email"
                                     {...register('streetAdd', { required: true })}
                                 />
-                                {errors.streetAdd && <span className="error">Full Name is required</span>}
+                                {errors.streetAdd && <span className="error">Address Required</span>}
 
                                 <input
                                     type="text"
                                     name="city"
                                     id="city"
-                                    placeholder="CITY"
+                                    placeholder="City"
                                     className="city"
                                     {...register('city', { required: true })}
                                 />
-                                {errors.city && <span className="error">Full Name is required</span>}
-                                <div className="mglf158 mgtop15">
+                                {errors.city && <span className="error"> City Required</span>}
+                                <div >
                                     <input
+                                        
                                         type="text"
                                         name="state"
                                         id="state"
                                         placeholder="State"
-                                        className="city"
+                                        className="city mgtop15"
                                         {...register('state', { required: true })}
                                     />
-                                    {errors.state && <span className="error">Full Name is required</span>}
+                                    {errors.state && <span className="error">State Required</span>}
                                     <input
                                         type="text"
                                         name="country"
                                         id="country"
                                         placeholder="Country"
-                                        className="city"
+                                        className="city mgtop15"
                                         {...register('country', { required: true })}
                                     />
-                                    {errors.country && <span className="error">Full Name is required</span>}
+                                    {errors.country && <span className="error">Country Required</span>}
                                     <input
                                         type="text"
                                         name="postalCode"
                                         id="postalCode"
                                         placeholder="Postal Code"
-                                        className="city"
+                                        className="city mgtop15"
                                         {...register('code', { required: true })}
                                     />
-                                    {errors.code && <span className="error">Full Name is required</span>}
+                                    {errors.code && <span className="error">Postal Code Required</span>}
                                 </div>
                             </div>
                             <div className="mgtop15">
-                                <label htmlFor="fullname">PHONE NUMBER (HOME)</label>
+                                <label htmlFor="fullname">PHONE NUMBER</label><br/>
                                 <input
+                                    className="mgtop15"
                                     type="number"
                                     name="num1"
                                     id="num1"
-                                    placeholder="+234 802 345 6789"
+                                    placeholder="Phone Number (Home)"
                                     {...register('num1', { required: true })}
                                 />
-                                {errors.num1 && <span className="error">Full Name is required</span>}
+                                {errors.num1 && <span className="error">Home Phone Required</span>}
                                 <button type="reset">
                                     <img src={editPen} alt="" />
                                 </button>
-                            </div>
-                            <div className="mgtop15">
-                                <label htmlFor="fullname">PHONE NUMBER (WORK)</label>
+                            
                                 <input
                                     type="text"
                                     name="num2"
                                     id="num2"
-                                    placeholder="+234 802 345 6789"
+                                    placeholder="Phone Number (Work)"
                                     {...register('num2', { required: true })}
                                 />
-                                {errors.num2 && <span className="error">Full Name is required</span>}
+                                {errors.num2 && <span className="error">Work Phone Required Required</span>}
                                 <button type="reset">
                                     <img src={editPen} alt="" />
                                 </button>
